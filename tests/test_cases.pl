6681d8579e7eaefafb547ab93767e918a5139da2
Harry Lloyd <147530886+Lloydster118@users.noreply.github.com>
2025-12-12 20:50:18 +0000

    testing

--- tests/test_cases.pl
+++ tests/test_cases.pl
@@ -1,2 +1,24 @@
-?- [src/gym_expert].
-?- advise.
+:- module(test_cases, [run_tests/0]).
+
+:- use_module('../src/gym_expert').
+
+run_tests :-
+    writeln('--- Running Gym Expert System Tests ---'),
+    test_1,
+    test_2,
+    test_3,
+    test_4,
+    test_5,
+    writeln('--- All tests executed ---').
+
+has_action(Key, Advice) :-
+    member(action(Key, _), Advice).
+
+get_action(Key, Advice, Value) :-
+    member(action(Key, Value), Advice).
+
+assert_true(Cond, Name) :-
+    (   call(Cond)
+    ->  format('PASS: ~w~n', [Name])
+    ;   format('FAIL: ~w~n', [Name])
+    ).
